53520eedc6bbabca7271ccdbbb89e7185fea5aa9
DoctorGreeb <dr.dre.odyssey@gmail.com>
2024-11-27 00:13:42 +0300

    add some bg

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
 
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX / window.innerWidth - 0.5; // Нормализуем координату X
+    const y = e.clientY / window.innerHeight - 0.5; // Нормализуем координату Y
+
+    const background = document.querySelector('.background');
+    background.style.transform = `translate(${x * 25}px, ${y * 25}px)`; // Перемещение фона в зависимости от курсора
+});
+
+
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX / window.innerWidth + 0.5; // Нормализуем координату X
+    const y = e.clientY / window.innerHeight + 0.5; // Нормализуем координату Y
+
+    const background = document.querySelector('.background2');
+    background.style.transform = `translate(${x * -25}px, ${y * -25}px)`; // Перемещение фона в зависимости от курсора
+});
